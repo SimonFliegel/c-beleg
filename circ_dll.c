8da1d4e05949366f25ef36625ef77526271125ba
SimonFliegel <simonfliegel@gmail.com>
2022-12-21 12:10:13 +0100

    finished building circular doubly linked list API

---
+++ circ_dll.c
@@ -0,0 +1,180 @@
+/* Name:
+MatrNr.: */
+
+/*****************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "circ_dll.h"
+
+typedef struct node {
+    void* pData;
+    struct node* pNext;
+    struct node* pPrev;
+} Node;
+
+typedef struct list{
+    Node head; // data=NULL, next=first, prev=last
+    Node* pCurr;
+} List;
+
+/********************* HELPER FUNCTIONS ***********************/
+
+// private
+int insertBehindNode(List* pList, Node* pPrev, void* pNewData) {
+    if (pPrev && pNewData) {
+        Node* pNode = malloc(sizeof(Node));
+        if (pNode) {
+            // configure node to be inserted
+            pNode->pData = pNewData;
+            pNode->pNext = pPrev->pNext;
+            pNode->pPrev = pPrev;
+            // set next-pointer of previous element
+            pPrev->pNext = pNode;
+            // set previous-pointer of next element
+            pNode->pNext->pPrev = pNode;
+            // set current-pointer to inserted node
+            pList->pCurr = pNode;
+            
+            return OK;
+        }
+    }
+    return FAIL;
+}
+
+// private
+int removeNode(Node* pNode) {
+    if (pNode) {
+        pNode->pPrev->pNext = pNode->pNext;
+        pNode->pNext->pPrev = pNode->pPrev;
+        free(pNode);
+        return OK;
+    }
+    return FAIL;
+}
+
+int isEmpty(List* pList) {
+    if (pList->head.pNext == (Node*)pList) {
+        return 1;
+    }
+    return 0;
+}
+
+/************************ FUNCTIONS **************************/
+
+List* createList() {
+    List* pList = malloc(sizeof(List));
+    if (pList) {
+        pList->head.pNext = (Node*)pList; // initialized pointing to itself
+        pList->head.pPrev = (Node*)pList;
+        pList->head.pData = NULL; // identification of head
+        pList->pCurr = NULL;
+    } 
+    return pList;
+}
+
+int insertAfter(List* pList, void* pNewData) {
+    return insertBehindNode(pList, pList->pCurr, pNewData);
+}
+
+int append(List* pList, void* pNewData) {
+    return insertBehindNode(pList, pList->head.pPrev, pNewData);
+}
+
+int push(List* pList, void* pNewData) {
+    return insertBehindNode(pList, &pList->head, pNewData);
+}
+
+int removeCurrent(List* pList) {
+    pList->pCurr = pList->pCurr->pPrev;
+    return removeNode(pList->pCurr->pNext);
+}
+
+int removeLast(List* pList) {
+    pList->pCurr = pList->head.pPrev->pPrev;
+    return removeNode(pList->head.pPrev);
+}
+
+int removeFirst(List* pList) {
+    pList->pCurr = pList->head.pNext->pNext;
+    return removeNode(pList->head.pNext);
+}
+
+void* getCurrent(List* pList) {
+    if (pList->pCurr == NULL) {
+        printf("ERROR::%d::%s: pCurr==NULL\n", __LINE__, __FILE__);
+        return NULL;
+    }
+    return pList->pCurr->pData;
+}
+
+void* getFirst(List* pList) {
+    if (isEmpty(pList)) {
+        printf("ERROR::%d::%s: list is empty\n", __LINE__, __FILE__);
+        return NULL;
+    }
+    pList->pCurr = pList->head.pNext;
+    return pList->pCurr->pData;
+}
+
+void* getLast(List* pList) {
+    if (isEmpty(pList)) {
+        printf("ERROR::%d::%s: list is empty\n", __LINE__, __FILE__);
+        return NULL;
+    }
+    pList->pCurr = pList->head.pPrev;
+    return pList->pCurr->pData;
+}
+
+void* getNext(List* pList) {
+    if (pList->pCurr == NULL) {
+        printf("ERROR::%d::%s: pCurr==NULL\n", __LINE__, __FILE__);
+        return NULL;
+    }
+    pList->pCurr = pList->pCurr->pNext;
+    return pList->pCurr->pPrev->pData;
+}
+
+// head not included in indexing
+void* getIndexed(List* pList, int index) {
+    if (isEmpty(pList)) {
+        printf("ERROR::%d::%s: list is empty\n", __LINE__, __FILE__);
+        return NULL;
+    }
+    pList->pCurr = pList->head.pNext;
+    for (int i = 1; i <= index; i++) {
+        pList->pCurr = pList->pCurr->pNext;
+        if (pList->pCurr->pData == NULL) {
+            printf("ERROR::%d::%s: index out of bounds\n", __LINE__, __FILE__);
+            return NULL;
+        }
+    }
+    return pList->pCurr->pData;
+}
+
+int getLength(List* pList) {
+    Node* pTmpCurr = pList->pCurr; 
+    int n = 0;
+    if (isEmpty(pList)) {
+        return 0;
+    }
+    pList->pCurr = pList->head.pNext;
+    while (pList->pCurr->pData) {
+        n++;
+        pList->pCurr = pList->pCurr->pNext;
+    }
+    pList->pCurr = pTmpCurr;
+    return n;
+}
+
+void deleteList(List* pList) {
+    pList->pCurr = pList->head.pNext;
+    while (pList->pCurr->pData) {
+        pList->pCurr = pList->pCurr->pNext;
+        removeNode(pList->pCurr->pPrev);
+    }
+    free(pList);
+}
+
+
+
---
+++ circ_dll.h
@@ -0,0 +1,122 @@
+/* Name:
+MatrNr.: */
+
+/**********************************************************/
+
+#define OK 1
+#define FAIL 0
+
+struct list;
+typedef struct list List;
+
+/**
+ * Inizializes list and creates head node containing:
+ * - next-pointer to first node
+ * - previous-pointer to last node
+ * - current-pointer to current node
+ * Next- and previous-pointer are both initially pointing
+ * to the list itself. Current-pointer is initially NULL.
+ * @return List* to created List
+*/
+List* createList();
+
+/**
+ * @param pList pointer to list
+ * @return void
+*/
+void deleteList(List* pList);
+
+/**
+ * Inserts new node after the current node and sets current node to newly inserted node
+ * @param pList pointer to list
+ * @param pNewData void* to data which should be inserted
+ * @return 1 -> OK, 0 -> FAIL
+ * 
+*/
+int insertAfter(List* pList, void* pNewData);
+
+/**
+ * Inserts new node at the end and sets current node to newly inserted node.
+ * @param pList pointer to list
+ * @param pNewData void* to data to be inserted
+ * @return 1 -> OK, 0 -> FAIL
+*/
+int append(List* pList, void* pNewData);
+
+/**
+ * Inserts new node at the front and sets current node to newly inserted node.
+ * @param pList pointer to list
+ * @param pNewData void* to data to be inserted
+ * @return 1 -> OK, 0 -> FAIL
+*/
+int push(List* pList, void* pNewData);
+
+/**
+ * Removes current node and sets current node to the previous node.
+ * @param pList pointer to list
+ * @param pNewData void* to data to be inserted
+ * @return 1 -> OK, 0 -> FAIL
+*/
+int removeCurrent(List* pList);
+
+/**
+ * Removes last node and sets current node to the new last node.
+ * @param pList pointer to list
+ * @return 1 -> OK, 0 -> FAIL
+*/
+int removeLast(List* pList);
+
+/**
+ * Removes first node and sets current node to the new first node.
+ * @param pList pointer to list
+ * @return 1 -> OK, 0 -> FAIL
+*/
+int removeFirst(List* pList);
+
+/**
+ * @param pList pointer to list
+ * @return void* to data of current node
+*/
+void* getCurrent(List* pList);
+
+/**
+ * Returns first node and sets current pointer to it.
+ * @param pList pointer to list
+ * @return void* to data of first node
+*/
+void* getFirst(List* pList);
+
+/**
+ * Returns last node and sets current pointer to it.
+ * @param pList pointer to list
+ * @return void* to data of last node
+*/
+void* getLast(List* pList);
+
+/**
+ * Returns next node and sets current pointer to it.
+ * @param pList pointer to list
+ * @return void* to data of next node
+*/
+void* getNext(List* pList);
+
+/**
+ * @param pList pointer to list
+ * @return 1 - true, 0 - false
+*/
+int isEmpty(List* pList);
+
+/**
+ * @param pList pointer to list
+ * @return int length (head not included)
+*/
+int getLength(List* pList);
+
+/**
+ * Returns node of given index (head not included in indexing).
+ * @param pList pointer to list
+ * @param index
+ * @return void* to data of indexed node
+ * @exception index out of bounds -> NULL
+*/
+void* getIndexed(List* pList, int index);
